8af8766b0aedb7f2caa215334b54a5283cfe0784
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2021-11-16 18:41:58 +0530

    Added publisher and subscriber with timer

---
+++ SwiftUI_Evolve_2/TimerBootcamp.swift
@@ -0,0 +1,117 @@
+//
+//  TimerBootcamp.swift
+//  SwiftUI_Evolve_2
+//
+//  Created by Sivaram Yadav on 11/16/21.
+//
+
+import SwiftUI
+
+struct TimerBootcamp: View {
+    
+//    let timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
+    let timer = Timer.publish(every: 3.0, on: .main, in: .common).autoconnect()
+
+    // Current time:
+    /*
+    @State var currentDate: Date = Date()
+    var dateFormatter: DateFormatter {
+        let formater = DateFormatter()
+        //formater.dateStyle = .medium
+        formater.timeStyle = .medium
+        return formater
+    }
+   */
+    
+    // Countdown:
+    /*
+    @State var count: Int = 10
+    @State var finishedText: String? = nil
+    */
+
+    // Countdown to date
+    /*
+    @State var timeRemaining: String = ""
+    let futureDate: Date = Calendar.current.date(byAdding: .hour, value: 1, to: Date()) ?? Date()
+    
+    func updateTimeRemaining() {
+        let remaining = Calendar.current.dateComponents([.hour, .minute, .second], from: Date(), to: futureDate)
+//        let hour = remaining.hour ?? 0
+        let minute = remaining.minute ?? 0
+        let second = remaining.second ?? 0
+        
+//        timeRemaining = "\(hour):\(minute):\(second)"
+        timeRemaining = "\(minute) minutes, \(second) seconds"
+    }
+    */
+  
+    // Animation counter:
+    @State var count: Int = 1
+    
+    
+    
+    var body: some View {
+        
+        ZStack {
+            RadialGradient(
+                gradient: Gradient(colors: [Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1))]),
+                center: .center,
+                startRadius: 5,
+                endRadius: 500
+            )
+            .ignoresSafeArea()
+            
+//            Text(dateFormatter.string(from: currentDate))
+//            Text(finishedText ?? "\(count)")
+            //Text(timeRemaining)
+                //.font(.system(size: 100, weight: .semibold, design: .rounded))
+                //.foregroundColor(.white)
+                //.lineLimit(1)
+                //.minimumScaleFactor(0.1)
+            
+//            HStack(spacing: 15) {
+//                Circle()
+//                    .offset(y: count == 1 ? -20 : 0)
+//                Circle()
+//                    .offset(y: count == 2 ? -20 : 0)
+//                Circle()
+//                    .offset(y: count == 3 ? -20 : 0)
+//            }
+//            .frame(width: 200)
+//            .foregroundColor(.white)
+            
+            TabView(selection: $count,
+                    content:  {
+                        Rectangle()
+                            .foregroundColor(.red)
+                            .tag(1)
+                        Rectangle()
+                            .foregroundColor(.blue)
+                            .tag(2)
+                        Rectangle()
+                            .foregroundColor(.green)
+                            .tag(3)
+                        Rectangle()
+                            .foregroundColor(.yellow)
+                            .tag(4)
+                        Rectangle()
+                            .foregroundColor(.orange)
+                            .tag(5)
+                    })
+                .frame(height: 200)
+                .tabViewStyle(PageTabViewStyle())
+        }
+        .onReceive(timer, perform: { _ in
+//            updateTimeRemaining()
+            withAnimation(.default) {
+                count = count == 5 ? 1 : count+1
+            }
+        })
+    }
+}
+
+struct TimerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerBootcamp()
+    }
+}
